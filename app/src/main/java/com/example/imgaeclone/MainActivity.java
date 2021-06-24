b722f2a021a207555fe37ab7f99134461b07d32c
Yu-bo, Zhang <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2021-06-24 17:58:29 +0800

    [Create] Init

---
+++ app/src/main/java/com/example/imgaeclone/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.imgaeclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.imgaeclone.databinding.ActivityMainBinding;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.Features2d;
+import org.opencv.features2d.SIFT;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    // make bitmap from image resource
+    private Bitmap inputImage;
+    private SIFT sift = SIFT.create();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java4");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+
+        inputImage = BitmapFactory.decodeResource(getResources(), R.drawable.girl);
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+        sift();
+    }
+
+    public void sift() {
+        Mat rgba = new Mat();
+        Utils.bitmapToMat(inputImage, rgba);
+        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+        Imgproc.cvtColor(rgba, rgba, Imgproc.COLOR_RGBA2GRAY);
+        sift.detect(rgba, keyPoints);
+        Features2d.drawKeypoints(rgba, keyPoints, rgba);
+        Utils.matToBitmap(rgba, inputImage);
+        imageView.setImageBitmap(inputImage);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
